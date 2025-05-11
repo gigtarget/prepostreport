98028bd84dd69f55898535ec1205bcd0108e03fc
gigtarget <jatingots9@gmail.com>
2025-05-11 16:27:41 -0400

    Update image_templates.py

--- utils/image_templates.py
+++ utils/image_templates.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pytz
 import os
 
-FONT_PATH = "fonts/Playfair.ttf"  # Ensure this matches your uploaded font
+FONT_PATH = "fonts/Agrandir.ttf"  # Ensure this matches your uploaded font
 
 def get_current_date_ist():
     ist = pytz.timezone("Asia/Kolkata")
