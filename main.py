6d3313dabc2cf8a1bac6bd99db0c4715c24b3240
gigtarget <jatingots9@gmail.com>
2025-05-11 16:29:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         font_size=60,
         text_color="black",
         start_y=300,
-        line_spacing=90,
+        line_spacing=110,
         start_x=100
     )
     send_telegram_file("output/report_image.jpg", "📊 Market Index Summary")
